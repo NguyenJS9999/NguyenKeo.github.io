e1c761822557958ca629782c58f4461d672b77a7
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-03-14 21:36:18 +0700

    nopbtvn buoi 10 valuable, if else, switch case, sua lat vat- xong b10 c 12 45

--- day10/btvn/cau4.js
+++ day10/btvn/cau4.js
@@ -3,6 +3,14 @@ console.log("Câu 4:")
 const chuoi = "Tôi học lớp web14"
 
 function inChuoi(chuoi) {
-    let chuoiRutGon = chuoi.substring(0, 10)
-    console.log(chuoiRutGon)
+    let doDaiChuoi = chuoi.length
+    if (doDaiChuoi > 10) {
+        let inChuoi = chuoi.substr(0, 10)
+        console.log("In ra chuỗi kí tự hơn 10 chữ: ", inChuoi, "...")
+    } else if (doDaiChuoi <= 10) {
+        let inChuoi = chuoi
+        console.log("In ra chuỗi kí tự <= 10 chữ: ", inChuoi)
+
+    }
 }
+inChuoi(chuoi)
