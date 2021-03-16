0dba0b7bf60f840285c644b5bbd95f8267802255
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-03-16 12:19:59 +0700

    nopbtvn buoi 10 valuable, if else, switch case - xong b10 c 12 45 3 di copy(ngoan)1

--- day10/btvn/cau3.js
+++ day10/btvn/cau3.js
@@ -5,14 +5,11 @@ function sum_in_number(in_number) {
   if (in_number <= 0) {
     return;
   }
-
   let sum = 0;
-
   while (in_number > 0) {
     sum += in_number % 10;
     in_number = Math.floor(in_number / 10);
   }
   return sum;
 }
-
 console.log("Tổng các số các chữ số của số Nguyên đó là: ",sum_in_number(123));
