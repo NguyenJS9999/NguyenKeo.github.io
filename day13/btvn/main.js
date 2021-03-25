6b48031b6a7c143101e6fe5597722f21ad3a1507
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-03-25 10:43:35 +0700

    buoi 13 sua them, buoi 14

--- day13/btvn/main.js
+++ day13/btvn/main.js
@@ -25,78 +25,73 @@ function ageAvg(arrays) {
     let sum = 0;
     let avg = 0;
     for (let i of arrays) {
-        console.log(i.age)
+        // console.log(i.age)
         sum = sum + i.age 
     }
     console.log("sum:", sum)
     avg = sum / arrays.length 
     return avg
 }
-
 console.log("Avg", ageAvg(students))
 document.write("<br />", "Số tuổi trung bình của các học sinh là: ", ageAvg(students), "<br /><br />")
 
 console.log("Câu 2: ");
 document.write("Câu 2: ")
-//2. Cho 1 mảng các object chứa thông tin sinh viên dạng { name: ''Anh'', age: 20 }. Viết hàm sắp mảng theo tuổi học viên từ cao đến thấp.
-function sortArrayObject(student) {
-
-    // var student = [40, 100, 1, 5, 25, 10];
-    student.sort(function (a, b) {
+//2. Cho 1 mảng các object chứa thông tin sinh viên dạng { name: ''Anh'', age: 20 }. 
+// Viết hàm sắp mảng theo tuổi học viên từ cao đến thấp.
+// Cách 1: Dùng hàm
+function sortArrayObject(arr) {
+    arr.sort(function (a, b) {
         return b.age - a.age
     });
-   return student
-
-    // let count = "";
-    // let sort = 0;
-    // for (let i of student) {
-    //     // console.log(i.age)
-    //     count = i.age
-    //     // let sort = count
-
-    //     console.log("count", typeof count)
-
-    // }
-
-
-    // console.log(Object.values(student))
-    // for (let value of student ) {
-    // console.log(value)
-    // count = value.age.toString().split().sort()
-    // sortCount = count.sort()
-    // console.log( sortCount)
-    // let splitStr = count
-    // console.log(splitStr)
-    // count += value.age
-    // console.log(typeof count)
-    // }
-
+   return arr
 }
 console.log(sortArrayObject(students))
-
-console.log("Câu 3: ");
-document.write("<br /> Câu 3: ")
-
-arrayFail = [false, null, undefined, NaN, 0, "", 1, 2, "a", "b"]
-
+// Dùng vòng lặp for 
+function sortArrayObject(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < i; j++) {
+        if (arr[i].age > arr[j].age) {
+          let item = arr[i];
+          arr[i] = arr[j];
+          arr[j] = item;
+        }
+      }
+    }
+    return arr;
+  }
+console.log(sortArrayObject(students))
+//
+console.log("Câu 3: ");document.write("<br /> Câu 3: ")
+let arrayFail = [false, null, undefined, NaN, 0, "", 1, 2, "a", "b"]
+// Cách 1:  dùng vòng lặp for 
+function giaTri(array){
+    let newArr =[];
+    for(let i = 0;i<array.length;i++){
+        if(array[i]) newArr.push(array[i]);
+    }
+    return newArr
+}
+console.log(giaTri(arrayFail))
+// Cách 2: Dùng hàng filter 
+removedItemFail = (arrayFail) => {
+    arrayFail = arrayFail.filter((value) => value);
+    return arrayFail;
+};
+// Cách 3: Dùng vòng for loại trừ các giá trị sai
 // function removedItemFail(arrayFail) {
 // let deleteItem = 0;
 // for (let i in arrayFail) {
 //     console.log(i === 0)
 //     if (i === false || i === null || i === NaN || i === 0 || i === "") {
 //         deleteItem = delete arrayFail.i
-
 //     }
 // }
 // return arrayFail
-
 // }
 
-removedItemFail = (arrayFail) => {
-    arrayFail = arrayFail.filter((value) => value);
-    return arrayFail;
-};
-document.write("Các giá trị đúng sau khi loại bỏ còn lại là: ", removedItemFail(arrayFail));
 console.log("Các giá trị đúng sau khi loại bỏ còn lại là: ", removedItemFail(arrayFail))
+document.write("Các giá trị đúng sau khi loại bỏ còn lại là: ", removedItemFail(arrayFail));
+//
 console.log("Câu 4: ");
 document.write("<br /> Câu 4: ")
