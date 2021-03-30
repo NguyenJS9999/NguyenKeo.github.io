69f9ff0b6a10a7bb894e44afc17cfba186f96d86
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-03-31 01:11:59 +0700

    buoi muoi sau cuoi cung: BMI xong chin tam phan tram

--- day16/btvn/bmi/bmi.js
+++ day16/btvn/bmi/bmi.js
@@ -44,7 +44,7 @@ console.log(weight);
 //         notificationMessage.innerText = "Vui lòng chỉ nhập cân nặng trong khoảng từ 20 đến 200";
 //         return;
 //     }
-   
+
 // }
 
 
@@ -52,39 +52,40 @@ function CalculatedBMI() {
     // CheckNumberHeight()
     // CheckNumberWeight()
 
-    height = heightInput.value/100;
+    height = heightInput.value / 100;
     weight = weightInput.value;
 
-    // if( weight < 20 || weight > 200 || weight === "" || height < 100 || height > 200 || height === ""){
-    //     notificationError.innerText = "Thông số đầu vào không hợp lệ";
-    //     console.log("weight", weight)
-    //     console.log("height", height)
-    //     return;
+    console.log("weight", weight)
+    console.log("height", height)
 
-    // } else {
+    if (weight < 20 || weight > 200 || weight === "" ||
+        height < 1 || height > 2 || height === "" ) {
 
-        console.log(height)
-        console.log(weight)
-      
-        
-        let bmi = ( weight / ( height * height ) ).toFixed(1);
-        console.log(bmi)
+        notificationError.innerText = "Thông số đầu vào không hợp lệ !!!";   
+        return;
+
+    } else {
+        console.log("weight", weight)
+        console.log("height", height)
+       
+        let bmi = (weight / (height * height)).toFixed(1);
+        console.log("bmi", bmi)
 
         notificationMessage.innerHTML = `Chỉ số BMI của bạn là: ${bmi}`;
 
-        if ( bmi < 18.5 ) {
+        if (bmi < 18.5) {
             resultBMI.innerHTML = `Chỉ số BMI của bạn hiện tại là: ${bmi}`;
             notificationMessage.innerText = "Chỉ số BMI dưới 18.5 bạn đang thiếu cân.";
             doctorHelp.innerText = "Bạn cần phải đi khám bác sĩ và áp dụng một chế độ tập luyện thể thao, thể hình,  ăn uống đủ chất và đúng cách nhất để có thể tăng cân an toàn phù hợp, bảo đảm sức khỏe."
-        } else if ( bmi < 25 ) {
+        } else if (bmi < 25) {
             resultBMI.innerHTML = `Chỉ số BMI của bạn hiện tại là: ${bmi}`;
             notificationMessage.innerText = "Chỉ số BMI từ 18.5 đến 24.9 là trạng thái cơ thể bình thường.";
             doctorHelp.innerText = "Bạn cần phải đi khám bác sĩ và áp dụng một chế độ tập luyện thể thao, dinh dưỡng tốt nhất để có thể tăng cân an toàn, đảm bảo sức khỏe."
-        } else if ( bmi < 30 ) {
+        } else if (bmi < 30) {
             resultBMI.innerHTML = `Chỉ số BMI của bạn hiện tại là: ${bmi}`;
             notificationMessage.innerText = "Chỉ số BMI từ 25.0 đến 29.9 là dấu hiệu ban bắt đầu thừa cân.";
             doctorHelp.innerText = "Trạng thái thừa cân lúc này chưa quá đáng lo, vì bậy ngay lúc này bạn hãy sớm tìm phương pháp và chế độ giảm cân phù hợp để nhanh chóng đưa cơ thể chở lại vóc dáng cân đối thon thả, và phòng ngừa tiềm ẩn bệnh tật về sau."
-        } else if ( bmi > 30 ) {
+        } else if (bmi > 30) {
             resultBMI.innerHTML = `Chỉ số BMI của bạn hiện tại là: ${bmi}`;
             notificationMessage.innerText = "Chỉ số BMI bằng hoặc trên 30 thực sự được xem là quá béo phì.";
             doctorHelp.innerText = "Giờ ở cấp độ thừa cân béo phì như này, cơ thể bạn sẽ gây lên áp lực rất lơn tới hệ thống xương khớp, . Bạn nên đi khám bác sĩ gấp để được tư vấn kịp thời phương pháp, cách thức giảm cân phù hợp và tốt nhất. "
@@ -92,7 +93,7 @@ function CalculatedBMI() {
 
     }
 
-// }
+}
 
 function restart() {
     window.location.reload();
