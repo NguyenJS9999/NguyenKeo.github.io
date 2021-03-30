bf785ae740378416480b63d97d19f0ded9073f40
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-03-30 13:41:07 +0700

    buoi muoi sau nop btvn age

--- day16/age.js
+++ day16/age.js
@@ -1,23 +1,31 @@
-let day = new Date ();
-let year = day.getFullYear();
-function ageCount() {
-    let namSinh = document.getElementById("age").value;
-    if (namSinh <= 0 || namSinh > year) {
-        thongBao = "Xin moi nhap lai nam sinh"
-    } else {
-       tuoi = year - namSinh;
-       if ( tuoi > 121) {
-        thongBao = "Ban da qua gia"
-        
-       }
-       thongBao =  `Tuoi cua ban la ${year - namsinamSinhnh}`
-    }
-    document.getElementById("alert").innerHTML = thongbao
+let inputAge = document.querySelector(".age-input");
+let CalculateButton = document.querySelector(".calculator-button");
+let publicMessage = document.querySelector(".notification-message");
 
+function AgeCalculation() {
+  let  YearOfBirth  = parseInt(inputAge.value);
+  console.log(typeof  YearOfBirth  )
+
+  let timeNow = new Date();
+  let  TheCurrentYear  = timeNow.getFullYear();
+
+  if ( YearOfBirth  < 0 || Number.isInteger( YearOfBirth ) === false ) {
+    publicMessage.innerText = "Số năm sinh nhập vào không hợp lệ!";
+    publicMessage.style.color = "#fa0536";
+  } 
+  else if ( YearOfBirth  < 1900 ||  YearOfBirth  >  TheCurrentYear ) {
+    publicMessage.innerText = "Vui lòng nhập năm sinh trong khoảng cho phép từ năm 1900 đến năm" +  TheCurrentYear  + "!";
+    publicMessage.style.fontSize = "20px";
+    publicMessage.style.color = "#ff0000";
+  } 
+  else {
+    let ageNumber =  TheCurrentYear  -  YearOfBirth ;
+    publicMessage.innerText = "Năm nay bạn đã" + ageNumber + " tuổi rồi.";
+    publicMessage.style.color = "inherit";
+    publicMessage.style.fontWeight = "900";
+  }
 }
 
-
-
-function reset() {
-    window.location.reload()
+function resetPage() {
+  location.reload();
 }
