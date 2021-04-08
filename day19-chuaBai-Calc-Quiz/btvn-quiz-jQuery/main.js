8f9f530a75be0e9be3daf25e839e67a595ae24f1
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-04-08 23:33:23 +0700

    Nôp btvn buoi 19 Quiz-Jquery giao diên, thêm chút logic thư?

--- day19-chuaBai-Calc-Quiz/btvn-quiz-jQuery/main.js
+++ day19-chuaBai-Calc-Quiz/btvn-quiz-jQuery/main.js
@@ -15,7 +15,11 @@ let answer = $('.option-1')[0].innerText;
 
 
 function choose_Answer(n) {
-    if ( n == 1 ) {
+    // if ( n == 1 ) {
+    //     console.log("Đáp án đúng")
+    // }
+
+    if ( answer == "Dùng text-align: center cho thẻ p chứa ảnh" ) {
         console.log("Đáp án đúng")
     }
     console.log(answer)
