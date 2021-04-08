6c4f7e2ad6dc33944487fb133367d207ce93bb5d
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-04-08 23:02:59 +0700

    Nôp btvn buoi 19 Quiz-Jquery giao diên

--- day19-chuaBai-Calc-Quiz/btvn-quiz-jQuery/main.js
+++ day19-chuaBai-Calc-Quiz/btvn-quiz-jQuery/main.js
@@ -1,11 +1,17 @@
 console.log("Quiz-jquery")
 
+// let option = document.getElementById("content")
+let option = document.getElementsByClassName("option-1")
+
+
+console.log(option.innerHTML)
+
 let question = [
     
 ]
 
-$(document).ready(function(){
-    $("div").click(function(){
-      $(this).hide();
-    });
-  });
+// $(document).ready(function(){
+//     $("div").click(function(){
+//       $(this).hide();
+//     });
+//   });
