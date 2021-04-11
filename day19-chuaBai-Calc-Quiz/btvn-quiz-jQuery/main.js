07960ad182c687f60887996b7f6bfe506ecb8a23
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-04-11 23:10:21 +0700

    btvn buoi 20 quiz lam truyen data obj len html loi css

--- day19-chuaBai-Calc-Quiz/btvn-quiz-jQuery/main.js
+++ day19-chuaBai-Calc-Quiz/btvn-quiz-jQuery/main.js
@@ -1,11 +1,10 @@
 console.log("Quiz-jquery")
 
-let datas = [
-    {
+let datas = [{
         ask: "Trong các cách đặt tên biến sau, cách nào sai cú pháp?",
-        answer: "Dùng text-align: center cho thẻ p chứa ảnh",
+        answer: ["12degree", "first-name"],
         choices: [
-            "12degree", 
+            "12degree",
             "_variable",
             "$12",
             "first-name",
@@ -16,23 +15,32 @@ let datas = [
         ask: "Để căn giữa ảnh trong 1 đoạn văn thì ta làm thế nào?",
         answer: "Dùng text-align: center cho thẻ p chứa ảnh",
         choices: [
-            "Dùng text-align: center cho thẻ p chứa ảnh", 
+            "Dùng text-align: center cho thẻ p chứa ảnh",
             "Dùng text-align: center cho ảnh",
             "Dùng margin: auto cho thẻ p chứa ảnh",
             "Dùng margin: auto cho ảnh",
         ]
     },
-    // { // question
-    //     ask: "",
-    //     answer: "",
-    //     choices: [
-    //         "", 
-    //         "",
-    //         "",
-    //         "",
-    //         "",
-    //     ]
-    // },
+    { // question
+        ask: "Element có position: absolute sẽ lấy mốc theo element cha gần nhất có position là?",
+        answer: ["fixed", "relative", "absolute"],
+        choices: [
+            "static",
+            "fixed",
+            "relative",
+            "absolute",
+        ]
+    },
+    { // question
+        ask: "Để tạo ra 1 nút cho phép người dùng bấm vào để gửi dữ liệu form ta có thể dùng element nào?",
+        answer: ["input", "button"],
+        choices: [
+            "form",
+            "div",
+            "input",
+            "button",
+        ]
+    },
 
 ] // datas 
 
@@ -40,31 +48,33 @@ let datas = [
 let curQuestion = 0;
 
 // Function render ra từng câu hỏi
-function renderQuestion ( count ) {
+function renderQuestion(count) {
     let question = datas[count];
 
-  // Cập nhật title, nội dung câu hỏi:
-  $(".question-title").html(`Câu hỏi ${count + 1}/${datas.length}`);
+    // Cập nhật title, nội dung câu hỏi:
+    $(".question-title").html(`Câu hỏi ${count + 1}/${datas.length}`);
 
-  $(".question-content").html(`${question.ask}`);
+    $(".question-content").html(`${question.ask}`);
 
-  // Cập nhật các lựa chọn cho câu hỏi:
-  $(".choices-box")[0].innerHTML = "";
+    // Cập nhật các lựa chọn cho câu hỏi:
+    $(".choices-box")[0].innerHTML = "";
 
-  for (let i = 0; i < question.choices.length; i++) {
-    $(".choices-box")[0].innerHTML += `<div class="answer" id="choice-${count + 1}" onclick="choose_Answer(this)">
-    <p>${question.choices[i]}</p>
-   
-</div>
-             `;
-  }
+    for (let i = 0; i < question.choices.length; i++) {
+        $(".choices-box")[0].innerHTML +=
+            `<div class="answer" id="option-${count + 1}" onclick="choose_Answer(this)">
+                <p>${question.choices[i]}</p>
+            </div>`;
+
+    }
 
 }
 // Chạy hàm ngay luôn khi vào phần mềm
 renderQuestion(curQuestion);
 
-
-
+// Hàm kiểm tra kết quả được chọn trả về
+function choose_Answer (that) {
+        console.log("Lựa chọn số ", )
+}
 
 
 
