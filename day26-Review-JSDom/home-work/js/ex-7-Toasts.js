f9e7c29f70de829763d6a21a34e8a7c44072170a
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-05-11 23:11:59 +0700

    hoan thanh btvn buoi 27 bai 1 3 4 5 6 xong bai 5 progress xong bai toast

--- day26-Review-JSDom/home-work/js/ex-7-Toasts.js
+++ day26-Review-JSDom/home-work/js/ex-7-Toasts.js
@@ -1,41 +1,23 @@
-console.log("Modal")
-let modal = $('.modal');
+console.log("toast")
 
+let setTimeOut;
 
-// Mở modal
-$(".btn-open").click(openModal);
-// Ấn nút mở hộp thoại, hiện thành block với màu nền xám xuyên thấu
-
-function openModal() {
-    console.log("Mở modal")
-
-    $(".btn-open").css("disabled");
-    $('.modal').css("display", "block")
-    $('.modal-backdrop').css("opacity", "1")
-    $('.modal-backdrop').css("display", "block")
-
-    setTimeout(function () {
-        $('.modal').css("display", "none")
-        $('.modal-backdrop').css("opacity", "0")
-        $('.modal-backdrop').css("display", "none")
-
-    }, 5000);
-
-}
-
-// Đóng modal - dùng class close riêng
-$(".close").click(closeModal);
-
-function closeModal() {
-    $('.modal-backdrop').css("display", "none")
-    $('.modal').css("display", "none")
-}
-
-// Đóng modal khi click vào vùng ngoài khối Modal
-$(window).on('click', function (e) {
-    if ($(e.target).is('.modal')) {
-        modal.hide();
-        // css div phủ về trong suốt
-        $('.modal-backdrop').css("opacity", "0")
-    }
+// Mở toast
+$(".on-toast").click(function () {
+    console.log("Open toast")
+    $(".toast").slideDown(500);
+    $(".toast").css("display", "block")
+    $(".toast").css("opacity", "1")
+    // Tự động ẩn khối toast lên sau 5s, chạy vào hàm Ẩn Toast
+    setTimeOut = setTimeout(closeToast, 5000)
 });
+
+// Ẩn toast 
+$(".btn-close").click(closeToast);
+
+// Chủ động ẩn khối Toast cuộn lên
+function closeToast() {
+    $(".toast").slideUp(500);
+    $('.toast').css("display", "none")
+    clearTimeout(setTimeOut);
+}
