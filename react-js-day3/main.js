617c4ef67760241f25e7afecf46ddb4b74a8a89f
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-08-17 18:10:30 +0700

    Lam btvn react js buoi 4 them chuc ang filter theo gia

--- react-js-day3/main.js
+++ react-js-day3/main.js
@@ -68,37 +68,120 @@ function ProductList() {
       image:
         "https://ruouthuonghieu.com/wp-content/uploads/2020/05/xRM_03-300x300.png.pagespeed.ic.C9G0BOWg8T.webp",
     },
+
+    {
+      id: 6,
+      name: "F NegroAmaro",
+      price: 1200000,
+      image:
+        "https://sanhruou.com/media/4058/catalog/F-NegroAmaro.jpg",
+    },
+
+    {
+      id: 7,
+      name: "IRONSTONE PETITE SIRAH",
+      price: 498000,
+      image:
+        "https://sanhruou.com/media/1140/catalog/ironstone-petite-sirah.jpg",
+    },
+
+    {
+      id: 8,
+      name: "Chivas 25 year",
+      price: 5850000,
+      image:
+        "http://khoruou.com/Uploads/Products/2011/9/chivas-25years.jpg",
+    },
+
+    {
+      id: 9,
+      name: "Chivas 21 year",
+      price: 2450000,
+      image:
+        "http://khoruou.com/Uploads/Products/2010/12/chivas21.jpg",
+    },
+
+    {
+      id: 10,
+      name: "Johnnie Walker Black Label",
+      price: 670000,
+      image:
+        "http://khoruou.com/Uploads/Products/2012/11/Jonhnnie-wallker-black-label.jpg",
+    },
+
+    {
+      id: 11,
+      name: "Johnnie Whisky JW Blue label Magnum 43%",
+      price: 8700000,
+      image:
+        "https://ruousg.com/wp-content/uploads/2020/10/JW-Blue-label-Magnum-1750ml.jpg",
+    },
+
+    {
+      id: 12,
+      name: "Johnnie Walker Red Label",
+      price: 350000,
+      image:
+        "http://khoruou.com/Uploads/Products/2011/9/Johnnie-Walker-Red-Label.jpg",
+    },
   ];
   // State/SetState, Mảng được clone lọc bị duỗi [...PRODUCTS]
   const [stateInputValue, setStateInputValue] = React.useState("");
-  const [stateProduct, setStateProducts] = React.useState([...PRODUCTS]);
+  // const [stateSortValue, setStateSortvalue] = React.useState("default");
+  const [stateProduct, setStateProducts] = React.useState(PRODUCTS);
+
   // Lấy dữ liệu ô input ép kiểu chữ ko thể viết hoa
   function handleChange(event) {
-    setStateInputValue(event.target.value.toLowerCase().trim());
+    setStateInputValue(event.target.value);
   }
   // Hàm Search - Tìm kiếm sản phẩm theo tên
   function findProduct() {
     // Cách 1: For i
+    // let productSearch;
     // for (let i = 0; i < PRODUCTS.length; i++) {
-    //   if ( PRODUCTS[i].name.toLowerCase().trim() ==  stateInputValue.toLowerCase().trim() ) {
+    //   if (
+    //     PRODUCTS[i].name.toLowerCase().trim() ==
+    //     stateInputValue.toLowerCase().trim()
+    //   ) {
     //     console.log("Tìm thấy sản phẩm bạn cần", [PRODUCTS[i]]);
-    //   } 
-    //   else {
+    //     stateProduct = PRODUCTS[i];
+    //     // return productSearch;
+    //   } else {
     //     break;
     //   }
     // }
-    // setStateProducts(PRODUCTS[i]);
+    // setStateProducts(stateProduct);
 
     // Cách 2; Filter - includes
-    let productSearch = PRODUCTS.filter(
-      (nameProduct) => nameProduct.name.toLowerCase().includes(stateInputValue)
-      // === stateInputValue
+    let productSearch = [...PRODUCTS].filter((nameProduct) =>
+      nameProduct.name.toLowerCase().includes(stateInputValue.toLowerCase().trim())
     );
-
     // setStateProduct sp cập nhập giá trị vào biến được lọc theo stateProduct rồi
-    setStateProducts(productSearch);
     console.log("productSearch", productSearch);
+    setStateProducts(productSearch);
   }
+
+  // Lọc giá tăng dần
+  function filterPricesIncreaseUp() {
+    console.log("Giá tăng dần");
+    let filteredList = [...PRODUCTS]
+    filteredList = filteredList.sort((a, b) => a.price - b.price);
+    setStateProducts(filteredList);
+  }
+  // Lọc giá giảm dần
+  function filterPricesDecreaseDown() {
+    console.log("Giá giảm dần");
+    let filteredList = [...PRODUCTS]
+    filteredList = filteredList.sort((a, b) => b.price - a.price);
+    setStateProducts(filteredList);
+  }
+   // Xõa Lọc giá - Sắp xếp mặc định
+  function filterPricesdefault() {
+    console.log("Xóa lọc giá - Sắp xếp mặc định");
+    let filteredList = [...PRODUCTS]
+    setStateProducts(filteredList);
+  }
+
   // Hàm MAP là vòng lặp qua mảng vẽ sp theo State của mảng giới hạn(Search) mới PRODUCTS
   const productsElement = stateProduct.map((product) => (
     <Product
@@ -111,17 +194,36 @@ function ProductList() {
 
   return (
     <>
-      <div className=" search-product-input container ">
-        {/* <i onClick={searchProduct} className="fas fa-search   search-btn "> </i> */}
-        <i onClick={findProduct} className="fas fa-search   search-btn ">
-          {" "}
-        </i>
+      <div className="filters-search-container  container ">
+        <div className=" filters-container  ">
+          
+        <div onClick={filterPricesdefault} className=" filter-btn  prices-increase-default ">
+            Sắp xếp mặc định  <i className="fas fa-times"></i>
+          </div>
 
-        <input
-          onChange={handleChange}
-          placeholder="Tìm sản phẩm"
-          type="text"
-        ></input>
+          <div onClick={filterPricesIncreaseUp} className=" filter-btn  prices-increase-up-gradually ">
+            Giá tăng dần  <i className="fas fa-arrow-up"></i>
+          </div>
+
+          <div onClick={filterPricesDecreaseDown} className=" filter-btn  prices-decrease-down-gradually " >
+            Giá giảm dần  <i className="fas fa-arrow-down"></i>
+          </div>
+
+        </div>
+
+        <div className=" search-product-input ">
+          {/* <i onClick={searchProduct} className="fas fa-search   search-btn "> </i> */}
+          <i onClick={findProduct} className="fas fa-search   search-btn ">
+            {" "}
+          </i>
+
+          <input
+            onChange={handleChange}
+            placeholder="Tìm sản phẩm"
+            type="text"
+          ></input>
+        </div>
+
       </div>
 
       <div className=" products-container container ">
